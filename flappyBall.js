b897738b802b3fa540d5b4f5764aff74c8b3db27
June <june87lee@gmail.com>
2019-10-17 18:20:47 -0700

    0.02 overhaul: Complete refactor. Used module design pattern for cleaner code.

--- flappyBall.js
+++ flappyBall.js
@@ -35,7 +35,7 @@ var handOr = false;
 //             break;
 //     }
 // });
-document.addEventListener("keydown", moveUp());
+document.addEventListener("keydown", moveUp);
 function moveUp(){
     // draw();
     hY -= 30;
@@ -74,7 +74,7 @@ function draw(){
         // collision detection
         if( hX + hand.width >= pipe[i].x && hX <= pipe[i].x + pipeNorth.width && 
             (hY <= pipe[i].y + pipeNorth.height || hY+hand.height >= pipe[i].y+constant) || 
-            hY + hand.height >=  cvs.height - fg.height){
+            hY + hand.height >=  cvs.height - fg.height || hY<0){
             //ctx.fillText("GAME OVER "+score,15,cvs.height-10);
             //gameOver = true;
             location.reload(); // gameover is simple reload
@@ -86,9 +86,61 @@ function draw(){
     ctx.drawImage(fg,0,cvs.height - fg.height);
     ctx.drawImage(hand,hX,hY);
     hY += gravity;
-    ctx.fillStyle = "#000";
+    ctx.fillStyle = "#66FCF1";
     ctx.font = "30px Arial";
     ctx.fillText("Score : "+score,10,cvs.height-20);
-    requestAnimationFrame(draw);
+    //requestAnimationFrame(draw);
+    //animateWithFpsLock(60);
 }
-draw();
+
+
+function animateWithFpsLock(fps){
+    console.log("--IN: animateWithFpsLock");
+    var fpsInterval, then;
+    fpsInterval = 1000/fps;
+    then = Date.now();
+    animate(then,fpsInterval);
+}
+
+function animate(then,fpsInterval){
+    
+    //var fpsInterval, startTime, now, then, elapsed;
+
+    //fpsInterval = 1000/fps;
+    //then = Date.now();
+    //startTime = then;
+    console.log(" ++IN: animate with then "+ then);
+    //console.log(" ++IN: animate with elapsed "+ elapsed);
+    requestAnimationFrame(function(){
+        animate(then,fpsInterval);
+    });
+
+    var now = Date.now();
+    var elapsed = now - then;
+    console.log(" ++IN: animate with elapsed "+ elapsed);
+    
+    if(elapsed > fpsInterval){
+        console.log("  --in: if(elapsed...");
+        then = now - (elapsed % fpsInterval);
+        draw();
+    }
+    // draw();
+    // var fpsInterval = 1000 / fps;
+    // var nextTime = fpsInterval+timestamp;
+    // var currTime = Date.parse(new Date());
+
+    // while(currTime<=nextTime){
+    //     currTime = Date.parse(new Date());
+    // }
+    // animateWithFpsLock(fps,Date.parse(new Date()));
+}
+    //console.log("blaaaah");
+    //draw();
+    //var fpsInterval = 1000 / fps;
+    //var nextTime = fpsInterval+timestamp;
+
+    //while(Date.parse(Date())<=nextTime);
+    //animateWithFpsLock(fps,Date.parse(new Date()));
+
+//draw();
+animateWithFpsLock(60);
